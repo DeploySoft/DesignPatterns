fdbb39d812c9ac518d22721cf77190151bf39b65
J. Andres Boyaca <janbs.alzo@gmail.com>
2020-07-29 20:18:01 -0500

    Basic structure memento

--- BehavioralPatterns/Memento/States.java
+++ BehavioralPatterns/Memento/States.java
@@ -4,4 +4,13 @@ package BehavioralPatterns.Memento;
  * This is Memento
  */
 public class States {
+    private final String value;
+
+    public States(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
